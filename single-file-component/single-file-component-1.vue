d0f867075536453947fc0d7c65cc4938b4aa32f9
fancyhejianyang <18651436384@163.com>
2017-07-11 19:05:12 +0800

    单文件组件介绍

---
+++ single-file-component/single-file-component-1.vue
@@ -0,0 +1,19 @@
+<template type="jade">
+    <p>{{greeting}} World!</p>
+</template>
+<script>
+module.exports = {
+    data:function(){
+        return {
+            greeting : 'Hello'
+        }
+    }
+}
+</script>
+<style>
+p{
+    font-size: 2em;
+    text-align: center;
+}
+</style>
+
